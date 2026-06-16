b49ca5f55a8860ced8db4504b371d75397b0e905
j <wogus061@gmail.com>
2026-06-16 23:55:29 +0900

    feat: D-5 내 서재(ReadingLog) 모델 구현

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Profile, Interest
+from .models import User, Profile, Interest, ReadingLog
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Profile)
 admin.site.register(Interest)
+
+@admin.register(ReadingLog)
+class ReadingLogAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'status', 'rating', 'created')
+    list_filter = ('status',)
+    search_fields = ('user__username', 'book__title')
