c32763473547eff26458ba6db02e5cbdc02ae52c
chosh1485 <chosh1485@gmail.com>
2026-06-16 20:29:45 +0900

    feat: 라우터 + 상단 내비

---
+++ frontend/src/views/ProfileView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="view-container">
+    <h1>프로필</h1>
+    <p>여기는 프로필 화면입니다. (선호 장르 및 주 도서관 설정이 표시될 예정입니다.)</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.view-container {
+  padding: 20px;
+}
+</style>
