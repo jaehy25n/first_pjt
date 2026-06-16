df4e27d0d6480926a31d2a6e8ffc933485c565fc
j <wogus061@gmail.com>
2026-06-16 23:48:12 +0900

    feat: D-3 도서관 및 도서, 소장 모델 구현

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Library, Book, Holding
 
-# Register your models here.
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ('lib_code', 'name', 'region')
+    search_fields = ('name', 'lib_code')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('isbn13', 'title', 'author', 'publisher', 'kdc_code')
+    search_fields = ('title', 'author', 'isbn13')
+
+@admin.register(Holding)
+class HoldingAdmin(admin.ModelAdmin):
+    list_display = ('library', 'book', 'has_book', 'loan_available', 'snapshot_at')
+    list_filter = ('has_book', 'loan_available', 'library')
+    search_fields = ('book__title', 'book__isbn13')
