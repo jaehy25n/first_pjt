1695d3c84af5aec9786d2e5f1c27c0835f2fa8c3
j <wogus061@gmail.com>
2026-06-16 23:46:16 +0900

    feat: D-2 관심사 모델 구현

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Profile
+from .models import User, Profile, Interest
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Profile)
+admin.site.register(Interest)
--- accounts/models.py
+++ accounts/models.py
@@ -5,9 +5,17 @@ from django.conf import settings
 class User(AbstractUser):
     pass
 
+class Interest(models.Model):
+    name = models.CharField(max_length=100)
+    kdc_prefix = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     reading_goal = models.CharField(max_length=200, blank=True)
+    interests = models.ManyToManyField(Interest, related_name='profiles', blank=True)
     # primary_library = models.ForeignKey('recommend.Library', on_delete=models.SET_NULL, null=True, blank=True) # 나중에 연결
 
     def __str__(self):
