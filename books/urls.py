32e07ebc3e3d9997eeac6940c9cbaff6193797e5
j <wogus061@gmail.com>
2026-06-18 03:49:18 +0900

    feat: 도서 검색 API

--- books/urls.py
+++ books/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('libraries/', views.LibraryListView.as_view(), name='library_list'),
+    path('books/', views.BookListView.as_view(), name='book_list'),
 ]
