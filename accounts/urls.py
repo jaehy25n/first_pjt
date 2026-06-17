4e8f33d0cf6790309bfc58665ad2077bf88d685c
j <wogus061@gmail.com>
2026-06-18 05:52:33 +0900

    feat: 독서기록 API

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('profile/onboarding/', views.ProfileOnboardingView.as_view(), name='profile_onboarding'),
     path('interests/', views.InterestListView.as_view(), name='interest_list'),
+    path('library', views.LibraryLogView.as_view(), name='library_log'),
+    path('library/log', views.LibraryLogCreateView.as_view(), name='library_log_create'),
+    path('library/toggle-wish', views.LibraryToggleWishView.as_view(), name='library_toggle_wish'),
 ]
