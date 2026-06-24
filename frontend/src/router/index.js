5e601cc8a650d7eff0399406364aded6a7d032a8
j <wogus061@gmail.com>
2026-06-24 19:50:39 +0900

    refactor: 프로필탭 삭제 (관심사·주도서관 설정 제거) (D36)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SearchView from '../views/SearchView.vue'
 import LoginView from '../views/LoginView.vue'
-import ProfileView from '../views/ProfileView.vue'
 
 const routes = [
   {
@@ -24,11 +23,6 @@ const routes = [
     path: '/signup',
     name: 'signup',
     component: () => import('../views/SignupView.vue')
-  },
-  {
-    path: '/profile',
-    name: 'profile',
-    component: ProfileView
   },
   {
     path: '/onboarding',
